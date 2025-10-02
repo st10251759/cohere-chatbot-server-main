c97ca86d6ec4c7c2e00488688b75a8e94031cecc
Cameron Chetty <ST10251759@vcconnect.edu.za>
2025-10-02 17:54:39 +0200

    Server js update

--- server.js
+++ server.js
@@ -256,6 +256,7 @@ app.post('/holiday', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Listening on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
